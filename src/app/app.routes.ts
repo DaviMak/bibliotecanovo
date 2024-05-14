9c2bfc99bfd31586cd0de65f0b46aaa0e84bb935
DaviMak <davi.amak@gmail.com>
2024-05-13 23:03:13 -0300

    quase tudo pronto

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -5,12 +5,17 @@ import { LivrolistComponent } from './components/livro/livrolist/livrolist.compo
 import { compileClassDebugInfo } from '@angular/compiler';
 import { Component } from '@angular/core';
 import { AutorlistComponent } from './components/autor/autorlist/autorlist.component';
+import { LivrodetailsComponent } from './components/livro/livrodetails/livrodetails.component';
 
 export const routes: Routes = [
     {path: "", redirectTo: "login", pathMatch: "full"},
     {path: "login", component: LoginComponent},
     {path: "admin", component: PrincipalComponent, children:[
         {path: "livros", component: LivrolistComponent},
-        {path: "autor", component: AutorlistComponent}
+        {path: "autor", component: AutorlistComponent},
+        {path: "livros/new", component: LivrodetailsComponent},
+        {path: "livros/edit/:id", component: LivrodetailsComponent},
+        {path: "autor/edit/:id", component: AutorlistComponent},
+        {path: "autor/edit/:id", component: AutorlistComponent}
     ]}
 ];
